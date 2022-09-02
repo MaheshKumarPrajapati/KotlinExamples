afac346da05b6b54a1a03cff7a084ca496bd5f3c
Mahesh Prajapati <maheshprajapati1990@gmail.com>
2022-09-02 11:13:18 +0530

    coroutine

---
+++ app/src/main/java/com/mahesh_prajapati/kotlin_examples/coroutine/AsyncBuilder.kt
@@ -0,0 +1,18 @@
+package com.mahesh_prajapati.kotlin_examples.coroutine
+import kotlinx.coroutines.*
+fun main() = runBlocking {    // Creates a blocking coroutine that executes in current thread (main)
+
+    println("Main program starts: ${Thread.currentThread().name}")  // main thread
+
+    val deferredJob: Deferred<Int> = async  {
+        // Thread: main
+    println("Fake work starts: ${Thread.currentThread().name}")     // Thread: main
+    delay(1000)   // Coroutine is suspended but Thread: main is free (not blocked)
+    println("Fake work finished: ${Thread.currentThread().name}") // Thread: main
+    15}
+
+
+     val num: Int = deferredJob.await()  // main thread: wait for coroutine to finish and return data
+
+    println("Main program ends: ${Thread.currentThread().name}")    // main thread
+}
